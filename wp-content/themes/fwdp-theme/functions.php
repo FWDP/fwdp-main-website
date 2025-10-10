5908f9915df37d62e234ead4c4d30200e47778dc
Peej Brion <peejbrion.filwebdevs@gmail.com>
2025-10-10 21:33:33 +0800

    Resolving issues from Theme.php file

--- wp-content/themes/fwdp-theme/functions.php
+++ wp-content/themes/fwdp-theme/functions.php
@@ -1,7 +1,40 @@
 <?php
-require_once get_template_directory() . '/inc/autoloader.php';
+// functions.php (top of file)
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
 
-$theme = new \FWDP\Core\Theme();
-add_action('after_setup_theme', function() use ($theme) {
-    $theme->init();
-});
+/**
+ * Autoloader
+ * Make sure your autoloader is required before we try to init classes.
+ * If you use Composer, this would typically be: require_once __DIR__ . '/vendor/autoload.php';
+ */
+$autoloader = get_template_directory() . '/inc/autoloader.php';
+if ( file_exists( $autoloader ) ) {
+    require_once $autoloader;
+}
+
+/**
+ * Boot the Theme (init components immediately)
+ * We prefer calling Theme::init() directly after the autoloader so
+ * components register their menu locations and supports before admin pages render.
+ */
+if ( class_exists( \FWDP\Core\Theme::class ) ) {
+    \FWDP\Core\Theme::init();
+} else {
+    // Fallback: try to require the Theme class file directly (if not autoloaded)
+    $theme_file = get_template_directory() . '/inc/Core/Theme.php';
+    if ( file_exists( $theme_file ) ) {
+        require_once $theme_file;
+        if ( class_exists( \FWDP\Core\Theme::class ) ) {
+            \FWDP\Core\Theme::init();
+        }
+    }
+}
+
+/**
+ * --- IMPORTANT ---
+ * If you previously had:
+ *   add_action('after_setup_theme', ['FWDP\\Core\\Theme','init']);
+ * remove or comment that line to avoid double initialization.
+ */
