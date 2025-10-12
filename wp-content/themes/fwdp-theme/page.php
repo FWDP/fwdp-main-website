533eb0cf7c8607c016a70e68615f17c803865ea5
Peej Brion <peejbrion.filwebdevs@gmail.com>
2025-10-12 13:22:44 +0800

    WIP: Homepage layout.

--- wp-content/themes/fwdp-theme/page.php
+++ wp-content/themes/fwdp-theme/page.php
@@ -2,6 +2,10 @@
 get_header();
 while ( have_posts() ) :
   the_post();
-  get_template_part( 'template-parts/content', 'page' );
+  if (is_front_page('home')){
+    get_template_part( 'template-parts/content', 'home' );
+  } else {
+    get_template_part( 'template-parts/content', 'page' );
+  }
 endwhile;
 get_footer();
