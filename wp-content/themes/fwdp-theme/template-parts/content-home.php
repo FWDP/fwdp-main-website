9b133df2f3ee4a1a8ab17a76cf153ddec0ad7374
Peej Brion <peejbrion.filwebdevs@gmail.com>
2025-10-15 14:40:05 +0800

    Added boilerplate on third part

--- wp-content/themes/fwdp-theme/template-parts/content-home.php
+++ wp-content/themes/fwdp-theme/template-parts/content-home.php
@@ -48,3 +48,51 @@
     </div>
 </section>
 <?php get_template_part('template-parts/service-grid', null, ['count' => 3, 'title' => 'What We Do', 'show_button' => false]); ?>
+<section class="bg-gray-100 py-12">
+    <div class="mx-auto md:max-w-screen-md lg:max-w-screen-lg px-4 sm:px-6 lg:px-8">
+        <h1 class="text-3xl font-extrabold text-gray-900 text-center">Our Culture</h1>
+        <div class="mt-12 flex grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 text-center">
+            <div>
+                <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0012 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75z" />
+                    </svg>
+
+                </div>
+                <div class="mt-5">
+                    <h3 class="text-lg font-medium text-gray-900">Mission</h3>
+                    <p class="mt-2 text-base text-gray-500">We aim to make the world a better place through innovation
+                        and collaboration.</p>
+                </div>
+            </div>
+            <div>
+                <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white text-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                    </svg>
+
+                </div>
+                <div class="mt-5">
+                    <h3 class="text-lg font-medium text-gray-900">Vision</h3>
+                    <p class="mt-2 text-base text-gray-500">We envision a world where technology is used to solve the
+                        most pressing issues facing humanity and improve people's lives.</p>
+                </div>
+            </div>
+        </div>
+        <h1 class="text-3xl font-extrabold text-gray-900 text-center mt-10 mb-10">Core Values</h1>
+        <div class="mx-auto md:max-w-screen-md lg:max-w-screen-lg px-4 sm:px-6 lg:px-8">
+            <ul class="text-center">
+                <li>Safety: A respectful, secure space for learning and collaboration.</li>
+                <li>Diversity & Inclusion: Welcoming all backgrounds and experiences.</li>
+                <li>Service: Giving back to uplift peers and the community.</li>
+                <li>Teamwork: Building stronger solutions through collaboration.</li>
+                <li>Fairness: Equal opportunities for every developer.</li>
+            </ul>
+        </div>
+    </div>
+</section>
